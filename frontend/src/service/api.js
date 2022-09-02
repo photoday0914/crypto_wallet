7b9c7268ba642131fcd2948c61c7602840269b80
photoday0914 <photoday0914@protonmail.com>
2022-09-02 05:47:50 -0400

    0902-1

---
+++ frontend/src/service/api.js
@@ -0,0 +1,61 @@
+// import ethers.js
+import Artifact from "../contracts/EtherWallet.json";
+
+const ethers = require("ethers");
+
+export function sendEther(privateKey, receiverAddress, amountInEther) {
+    let network = "goerli";
+    const Confirmations = 2;
+    // provider: Infura or Etherscan will be automatically chosen
+    // let provider = ethers.getDefaultProvider(network);
+    let provider = new ethers.providers.EtherscanProvider(
+        network,
+        "FNEQ5NPS4E1DMTCREUQW3RP3Y8ZXJVJEVY"
+    );
+
+    // Create a wallet instance
+    let wallet = new ethers.Wallet(privateKey, provider);
+
+    // Create a transaction object
+    let tx = {
+        to: receiverAddress,
+        // Convert currency unit from ether to wei
+        value: ethers.utils.parseEther(amountInEther),
+        gasLimit: 50000,
+
+        // nonce: nonce || undefined,
+    };
+    // Send a transaction
+    wallet.sendTransaction(tx).then((txObj) => {
+        console.log("txHash", txObj.hash);
+        provider.waitForTransaction(txObj.hash, Confirmations);
+        wallet.getBalance().then((data) => {
+            console.log(data.toString());
+        });
+        // => 0x9c172314a693b94853b49dc057cf1cb8e529f29ce0272f451eea8f5741aa9b58
+        // A transaction result can be checked in a etherscan with a transaction hash which can be obtained here.
+    });
+}
+
+export function getBlackList() {}
+
+export function pushBlackList() {
+    let network = "goerli";
+    let provider = new ethers.providers.EtherscanProvider(
+        network,
+        "FNEQ5NPS4E1DMTCREUQW3RP3Y8ZXJVJEVY"
+    );
+    // console.log(ethers.Contract());
+    const deployedContract = new ethers.Contract(
+        "0x92713824d3b654aDED6609272B1C2631eFA89f7C", //deployed contract address
+        Artifact.abi,
+        provider //provider:only read, signer:can execute state-changing operation
+    );
+
+    deployedContract.getBlackList().then((data) => {
+        console.log("blacklist: ", data);
+    });
+    deployedContract.getBalance().then((data) => {
+        console.log("balance: ", data.toString());
+    });
+}
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,5 +1,6 @@
 // const ethers = require("hardhat");
 const { verify } = require("../utils/verify");
+const { artifacts } = require("hardhat");
 
 async function main() {
     const [deployer] = await ethers.getSigners();
@@ -17,6 +18,28 @@ async function main() {
 
     console.log("Verifying...");
     await verify(etherWallet.address, []);
+    saveFrontendFiles(etherWallet);
+}
+
+function saveFrontendFiles(etherWallet) {
+    const fs = require("fs");
+    const contractsDir = __dirname + "/../frontend/src/contracts";
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        contractsDir + "/contract-address.json",
+        JSON.stringify({ address: etherWallet.address }, undefined, 2)
+    );
+
+    const Artifact = artifacts.readArtifactSync("EtherWallet");
+
+    fs.writeFileSync(
+        contractsDir + "/Artifact.json",
+        JSON.stringify(Artifact, null, 2)
+    );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
